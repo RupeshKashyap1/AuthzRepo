51427d8e01370d197918ef76306395a1691cf0d6
Tnluser <tnluser@Administrators-MacBook-Pro.local>
2022-12-01 02:12:09 +0530

    Initial Commit: Springboot client setup

---
+++ authzSystem/src/test/java/com/user/authzSystem/AuthzSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.authzSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthzSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
